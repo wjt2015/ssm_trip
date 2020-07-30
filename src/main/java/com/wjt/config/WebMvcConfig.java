8f746e1478ea1296af9881c798dc1200be0b57a1
王锦涛 <jintao9@staff.weibo.com>
2020-07-30 22:34:31 +0800

    add_registry

--- src/main/java/com/wjt/config/WebMvcConfig.java
+++ src/main/java/com/wjt/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.MyDelegatingWebMvcConfiguration;
 
 /**
+ * (https://www.bilibili.com/video/av56616279?p=5);
  * @Time 2020/1/31/17:47
  * @Author jintao.wang
  * @Description
---
+++ src/test/java/com/wjt/dao/LoginMapperTest.java
@@ -0,0 +1,43 @@
+package com.wjt.dao;
+
+import com.wjt.config.DaoConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {DaoConfig.class})
+public class LoginMapperTest {
+    @Resource
+    private LoginMapper loginMapper;
+
+    @Test
+    public void deleteByPrimaryKey() {
+    }
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void insertSelective() {
+    }
+
+    @Test
+    public void selectByPrimaryKey() {
+    }
+
+    @Test
+    public void updateByPrimaryKeySelective() {
+    }
+
+    @Test
+    public void updateByPrimaryKey() {
+    }
+}
